e0f6ea995c8cd9febd86e4d2a663d81dec04c6f8
Jamil Apicela <trab.jamilsouza@gmail.com>
2025-09-13 11:57:38 -0300

    feat:  do comment and remove comment

--- src/main/java/apicela/notstagram/mappers/CommentMapper.java
+++ src/main/java/apicela/notstagram/mappers/CommentMapper.java
@@ -3,6 +3,9 @@ package apicela.notstagram.mappers;
 import apicela.notstagram.models.dtos.CommentDTO;
 import apicela.notstagram.models.dtos.UserSummaryDTO;
 import apicela.notstagram.models.entities.Comment;
+import apicela.notstagram.models.entities.Post;
+import apicela.notstagram.models.entities.User;
+import apicela.notstagram.models.requests.CommentRequest;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -17,6 +20,14 @@ public class CommentMapper {
                 comment.getCreatedAt(),
                 new UserSummaryDTO(comment.getUser().getUsername())
         );
+    }
+
+    public Comment toEntity(CommentRequest dto, User user, Post post) {
+        Comment comment = new Comment();
+        comment.setUser(user);
+        comment.setPost(post);
+        comment.setText(dto.text());
+        return comment;
     }
 
     public List<CommentDTO> toDTOList(List<Comment> comments) {
